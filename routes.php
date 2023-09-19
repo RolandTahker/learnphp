9e14594a83db28c32da78a01e6cff154750f38ae
RolandTahker <rolandtahker@gmail.com>
2023-09-19 11:45:28 +0300

    Php

--- routes.php
+++ routes.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Controllers\ArticlesController;
 use App\Router;
 use App\Controllers\PublicController;
 Router::get('/', [PublicController::class, 'home']);
@@ -7,3 +8,7 @@ Router::get('/about', [PublicController::class, 'about']);
 
 Router::get('/test', [PublicController::class, 'test']);
 Router::get('/test', [PublicController::class, 'testAnswer']);
+
+Router::get('/admin/articles', [ArticlesController::class, 'index']);
+Router::get('/admin/articles/new', [ArticlesController::class, 'create']);
+Router::post('/admin/articles', [ArticlesController::class, 'store']);
--- views/home.php
+++ views/home.php
@@ -1,23 +1,24 @@
     <?php include __DIR__ . '/partials/header.php'; ?>
 <div class="container">
     <div class="columns">
+        <?php foreach($articles as $article): ?>
         <div class="column is-3">
             <div class="card">
                 <header class="card-header">
                     <p class="card-header-title">
-                        Component
+                        <?=$article->title?>
                     </p>
                     </header>
                     <div class="card-content">
                         <div class="content">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                            <?=$article->body?>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        <?php endforeach; ?>
     </div>
-
-    <?php include __DIR__ . '/partials/footer.php'; ?>
+</div>
+<?php include __DIR__ . '/partials/footer.php'; ?>
 
 
