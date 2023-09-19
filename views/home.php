6eda654ad6745d05f0b452dff3a78e5595769ddd
RolandTahker <rolandtahker@gmail.com>
2023-09-19 09:44:51 +0300

    Update home view

--- views/home.php
+++ views/home.php
@@ -1,10 +1,23 @@
     <?php include __DIR__ . '/partials/header.php'; ?>
-    <h1><?=$name?></h1>
-    <?php if($num > 10):  ?>
-        <h1>Suurem</h1>
-    <?php  else:  ?>
-        <h1>Väiksem</h1>
-    <?php endif ?>
+<div class="container">
+    <div class="columns">
+        <div class="column is-3">
+            <div class="card">
+                <header class="card-header">
+                    <p class="card-header-title">
+                        Component
+                    </p>
+                    </header>
+                    <div class="card-content">
+                        <div class="content">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <?php include __DIR__ . '/partials/footer.php'; ?>
 
 
